81ea0422e3b8c38578503d49b652420720d745cf
Atlantyk <163650765+Yoourself@users.noreply.github.com>
2024-09-03 19:28:14 +0200

    Word List.py

---
+++ Word List.py
@@ -0,0 +1,10 @@
+slowa = {
+            "CRINGE": "Coś wyjątkowo dziwnego lub zawstydzającego",
+            "LOL": "Częsta reakcja na coś zabawnego",
+            "ROFL": "odpowiedź na żart",
+            "SHEESH" : "lekka dezaprobata",
+            "CREEPY" : "straszny,złowieszczy",
+            "AGGRO" : "stać się agresywnym/zły"
+            }
+slowo = input("podaj slowo")
+print(slowa[slowo])
